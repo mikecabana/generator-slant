7e0bfe8bf6b5a8565a5e589aaee7a9ffbaadf8f8
Michael Cabana <m.cabana@ieg-america.com>
2018-08-14 17:06:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Angular 6+ Module Generator
+
+> This [Yeoman](https://yeoman.io) generator lets you quickly generate a new module for an Angular CLI Project in a moduled approach.
+
+## Usage
+
+
+### Angular 6+
+
+To run this generator, first you need an `@angular/cli` generated project.
+
+Install the `@angular/cli` by running
+```
+npm i -g @angular/cli
+```
+
+then by generating a new project
+```
+ng new my-fancy-project
+```
+##### *See [Angular CLI docs](https://github.com/angular/angular-cli/wiki) for more info on generating angualr artifacts.
+
+### Yeoman
+
+Install `yo` and `generator-slant` globally by running
+
+```
+npm i -g yo generator-slant
+```
+
+The from the root of your angular project run
+```
+yo slant
+```
+
+
+
---
+++ generators/app/index.js
@@ -0,0 +1,100 @@
+'use strict'
+
+const Generator = require('yeoman-generator');
+const Chalk = require('chalk');
+
+module.exports = class extends Generator {
+
+    constructor(args, opts) {
+        super(args, opts);
+        this.option('babel');
+    }
+
+    initializing() {
+        this.log(Chalk.bold.magenta('Slanting!'));
+    }
+
+    async prompting() {
+        this.answers = await this.prompt([
+            {
+                type: 'list',
+                name: 'moduleType',
+                message: 'What type of module do you need?',
+                choices: ['blank', 'service', 'feature'],
+                default: 0
+            },
+            {
+                when: (response) => {
+                    return response.moduleType === 'service'
+                },
+                type: 'confirm',
+                name: 'httpModule',
+                message: 'Will this be a data access module?',
+                default: 0
+            },
+            {
+                type: 'input',
+                name: 'moduleName',
+                message: 'Name your module [do not have the word `module` in your name]:',
+                default: 'sample-' + Math.floor(Math.random() * 10000)
+            }
+        ]);
+    }
+
+    // configuring() { }
+
+    // default() { }
+
+    writing() {
+        if (this.answers.moduleType === 'service') {
+            if (this.answers.httpModule) {
+                this.fs.copy(
+                    this.templatePath('../../public/index-t.txt'),
+                    this.destinationPath(`src/app/${this.answers.moduleName}/abstraction/index.ts`),
+                );
+            } else {
+                this.fs.copy(
+                    this.templatePath('../../public/index-t.txt'),
+                    this.destinationPath(`src/app/${this.answers.moduleName}/services/index.ts`),
+                );
+            }
+        }
+        if (this.answers.moduleType === 'feature') {
+            this.fs.copy(
+                this.templatePath('../../public/index-t.txt'),
+                this.destinationPath(`src/app/${this.answers.moduleName}/abstraction/index.ts`),
+            );
+            this.fs.copy(
+                this.templatePath('../../public/index-t.txt'),
+                this.destinationPath(`src/app/${this.answers.moduleName}/view/components/index.ts`),
+            );
+            this.fs.copy(
+                this.templatePath('../../public/index-t.txt'),
+                this.destinationPath(`src/app/${this.answers.moduleName}/view/pages/index.ts`),
+            );
+        }
+    }
+
+    // conflicts() { }
+
+    install() {
+        if (this.answers.moduleType === 'blank') {
+            this.spawnCommandSync('ng', ['g', 'm', this.answers.moduleName]);
+        }
+        if (this.answers.moduleType === 'service') {
+            if (this.answers.httpModule) {
+                this.spawnCommandSync('ng', ['g', 'm', `${this.answers.moduleName}/dal/${this.answers.moduleName}-dal`, '--flat']);
+            } else {
+                this.spawnCommandSync('ng', ['g', 'm', `${this.answers.moduleName}/${this.answers.moduleName}`, '--flat']);
+            }
+        }
+        if (this.answers.moduleType === 'feature') {
+            this.spawnCommandSync('ng', ['g', 'm', `${this.answers.moduleName}/dal/${this.answers.moduleName}-dal`, '--flat']);
+            this.spawnCommandSync('ng', ['g', 'm', `${this.answers.moduleName}/view/${this.answers.moduleName}-view`, '--flat']);
+        }
+    }
+
+    end() {
+        this.log(Chalk.bold.magenta('Donezo!'));
+    }
+};
